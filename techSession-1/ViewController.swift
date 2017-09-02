2c3630f8a7b1bffb90e29c9397c04ee0f4c87bb5
~A <anilrm07@gmail.com>
2017-09-01 18:23:36 -0700

    mini app

--- techSession-1/ViewController.swift
+++ techSession-1/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
+    @IBOutlet weak var addButton: UIButton!
+    var digit = 0
+    var toggle = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Label.text = "\(digit)";
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func add(_ sender: AnyObject) {
+        //toggle the button title
+        if digit <= 0 {
+            addButton.setTitle("ADD", for: .normal);
+            addButton.setTitle("ADD", for: .focused);
+            addButton.setTitle("ADD", for: .highlighted);
+            addButton.setTitle("ADD", for: .selected);
+            addButton.setTitle("ADD", for: .disabled);
+            toggle = true
+        }else if digit >= 10{
+            addButton.titleLabel?.text = "Substract"
+            toggle = false
+            addButton.setTitle("SUBSTRACT", for: .normal);
+            addButton.setTitle("SUBSTRACT", for: .focused);
+            addButton.setTitle("SUBSTRACT", for: .highlighted);
+            addButton.setTitle("SUBSTRACT", for: .selected);
+            addButton.setTitle("SUBSTRACT", for: .disabled);
+        }
+        
+        if toggle {
+            digit = digit + 1
+        }else{
+            digit = digit - 1
+        }
+        
+        Label.text = "\(digit)";
+    }
 
 }
 
